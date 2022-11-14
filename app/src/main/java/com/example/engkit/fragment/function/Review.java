99b34b8adf51bb5cc0d4a081d69839c11bb7dd0b
hoang24012002a <79365731+hoang24012002a@users.noreply.github.com>
2022-11-14 10:56:22 +0700

    update frame Menu

--- app/src/main/java/com/example/engkit/fragment/function/Review.java
+++ app/src/main/java/com/example/engkit/fragment/function/Review.java
@@ -1,4 +1,4 @@
-package com.example.engkit.fragment;
+package com.example.engkit.fragment.function;
 
 import android.os.Bundle;
 
@@ -12,10 +12,10 @@ import com.example.engkit.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Rank#newInstance} factory method to
+ * Use the {@link Review#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Rank extends Fragment {
+public class Review extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class Rank extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Rank() {
+    public Review() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class Rank extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Rank.
+     * @return A new instance of fragment Review.
      */
     // TODO: Rename and change types and number of parameters
-    public static Rank newInstance(String param1, String param2) {
-        Rank fragment = new Rank();
+    public static Review newInstance(String param1, String param2) {
+        Review fragment = new Review();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class Rank extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rank, container, false);
+        return inflater.inflate(R.layout.fragment_review, container, false);
     }
 }
